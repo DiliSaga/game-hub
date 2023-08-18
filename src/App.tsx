d4e1b1cd8bb85584b22b0d85cd7ddcaad3f19aca
dili saga <sagadilshan@gmail.com>
2023-08-18 23:29:11 +0800

    Adding Chakra color mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 
+
 function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area="nav" bg="gold">
+      <GridItem area="nav" >
         <NavBar/>
       </GridItem>
       <Show above="lg">
